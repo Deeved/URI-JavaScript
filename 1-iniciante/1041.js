6c4d50ec8a697f680a59f738ea1372b5794f89a6
Deeved Hiuston <deeved@gmail.com>
2020-08-23 15:04:40 -0300

    1041 accepted

---
+++ 1-iniciante/1041.js
@@ -0,0 +1,19 @@
+const input = require('fs').readFileSync('stdin', 'utf8')
+
+let [x, y] = input.split(' ').map(item => parseFloat(item))
+
+if(x === 0  && y === 0){
+    console.log('Origem')
+} else if(x === 0){
+    console.log('Eixo Y')
+} else if(y === 0){
+    console.log('Eixo X')
+} else if(x > 0 && y > 0){
+    console.log('Q1')
+} else if(x < 0 && y > 0){
+    console.log('Q2')
+} else if(x < 0 && y < 0){
+    console.log('Q3')
+}else{
+    console.log('Q4')
+}
