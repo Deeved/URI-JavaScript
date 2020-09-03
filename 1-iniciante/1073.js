19bd7031e323f8b7e8cf32d44a4e5d76ad39e701
Deeved Hiuston <deeved@gmail.com>
2020-09-03 00:48:37 -0300

    1073 accepted

---
+++ 1-iniciante/1073.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync('stdin', 'utf8')
+
+let x = parseInt(input.split('\n'))
+let i = 2
+for(i; i<=x; i+=2){
+    if(i>1998) return
+    let result = i * i
+    console.log(`${i}^${2} = ${result}`)
+}
